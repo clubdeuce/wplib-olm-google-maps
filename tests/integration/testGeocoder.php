b88a63d8f5bf8029f25f965601ed77c23ac9a04d
Daryl Lozupone <daryl@newclarity.net>
2016-11-03 01:31:34 -0400

    Add geocoder tests

---
+++ tests/integration/testGeocoder.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Clubdeuce\WPLib\Components\GoogleMaps\Tests\Integration;
+
+use Clubdeuce\WPLib\Components\GoogleMaps\Geocoder;
+use Clubdeuce\WPLib\Components\GoogleMaps\Tests\TestCase;
+
+/**
+ * Class TestGeocoder
+ * @package Clubdeuce\WPLib\Components\GoogleMaps\Tests\Integration
+ * @coversDefaultClass Clubdeuce\WPLib\Components\GoogleMaps\Geocoder
+ */
+class TestGeocoder extends TestCase {
+
+    /**
+     * @var Geocoder;
+     */
+    private $_geocoder;
+
+    public function setUp() {
+        $this->_geocoder = new Geocoder(['api_key' => '']);
+    }
+
+    /**
+     * @covers ::_make_request
+     */
+    public function testMakeRequest() {
+        $url      = $this->reflectionMethodInvokeArgs($this->_geocoder, '_make_url', '1600 Amphitheatre Parkway, Mountain View, CA');
+        $response = $this->reflectionMethodInvokeArgs($this->_geocoder, '_make_request', $url);
+
+        $this->assertInternalType('array', $response);
+    }
+
+    /**
+     * @covers ::_make_request
+     */
+    public function testMakeRequestError404() {
+        $response = $this->reflectionMethodInvokeArgs($this->_geocoder, '_make_request', 'https://maps.googleapis.com/maps/api/geo');
+
+        $this->assertInstanceOf('WP_Error', $response);
+        $this->assertObjectHasAttribute('errors', $response);
+        $this->assertArrayHasKey('404', $response->errors);
+    }
+
+    /**
+     * @covers ::geocode
+     */
+    public function testGeocode() {
+        $result = $this->_geocoder->geocode('1600 Amphitheatre Parkway, Mountain View, CA');
+
+        $this->assertInternalType('array', $result);
+        $this->assertArrayHasKey('lat', $result);
+        $this->assertArrayHasKey('lng', $result);
+    }
+}
