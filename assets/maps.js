14dabf3d6b483d8dcafc9b6f44da6e59388aa48a
Daryl Lozupone <daryl@newclarity.net>
2016-12-28 14:41:42 -0500

    Add methods to get end user location via HTML 5 Geolocation API

--- assets/maps.js
+++ assets/maps.js
@@ -1,4 +1,10 @@
 var gmMaps = {};
+var geocoder = new google.maps.Geocoder;
+
+jQuery(document).ready(function($){
+   userLocation();
+});
+
 
 function generate_map(mapId, mapParams, mapMarkers, infoWindows) {
     var map = new google.maps.Map(document.getElementById(mapId), {
@@ -24,4 +30,54 @@ function generate_map(mapId, mapParams, mapMarkers, infoWindows) {
     });
 
     gmMaps[mapId] = {map: map, markers: markers, infoWindows: windows};
+}
+
+// Get the browser location using HTML 5 Geolocation
+function userLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            gmMaps["userLocation"] = {position: position};
+            addressFromLocation(position.coords.latitude, position.coords.longitude);
+        }, userLocationError);
+    }
+}
+
+function userLocationError(error) {
+    var errorMessage = '';
+    
+    switch(error.code) {
+        case error.PERMISSION_DENIED:
+            errorMessage = "User denied the request for Geolocation.";
+            break;
+        case error.POSITION_UNAVAILABLE:
+            errorMessage = "Location information is unavailable.";
+            break;
+        case error.TIMEOUT:
+            errorMessage = "The request to get user location timed out.";
+            break;
+        case error.UNKNOWN_ERROR:
+            errorMessage = "An unknown error occurred.";
+            break;
+    }
+
+    gmMaps['userLocation'] = {status: 'error', code: error.code, message: errorMessage};
+}
+
+/**
+ *
+ * @param lat
+ * @param lng
+ * @returns {*}
+ */
+function addressFromLocation(lat, lng) {
+    var geocoder = new google.maps.Geocoder;
+    var latLng   = new google.maps.LatLng(lat, lng);
+    var address  = '';
+    geocoder.geocode({'latLng': latLng}, function(results, status){
+        if (status == google.maps.GeocoderStatus.OK){
+            if(results[0]){
+                gmMaps['userLocation']['address'] = results[0].formatted_address;
+            }
+        }
+    });
 }
