9ebc8094cd6bfd520b01df79532708740f82fcdb
Daryl Lozupone <daryl@newclarity.net>
2017-01-05 22:52:40 -0500

    Refactor map generation to ensure the map zoom level incorporates all markers present on the map

--- assets/maps.js
+++ assets/maps.js
@@ -5,29 +5,47 @@ jQuery(document).ready(function($){
    userLocation();
 });
 
-
+/**
+ *
+ * @param mapId
+ * @param mapParams
+ * @param mapMarkers
+ * @param infoWindows
+ */
 function generate_map(mapId, mapParams, mapMarkers, infoWindows) {
     var map = new google.maps.Map(document.getElementById(mapId), {
         center: mapParams.center,
         zoom: Number(mapParams.zoom)
     });
     var markers = [];
+    var bounds = new google.maps.LatLngBounds();
+    var infoWindow = new google.maps.InfoWindow();
 
     jQuery.each(mapMarkers, function(key, object) {
         object.map = map;
-        markers.push(new google.maps.Marker(object));
-    });
+        var marker = new google.maps.Marker(object);
+        markers.push(marker);
 
-    jQuery.each(markers, function(key, marker) {
-        var infoWindow = new google.maps.InfoWindow({
-            content: infoWindows[key].content
-        });
-        windows.push(infoWindow);
+        // Add the position of the marker to the bounds object
+        bounds.extend(object.position);
+
+        // Add the info box open click listener
         marker.addListener('click', function(){
+            infoWindow.setContent(infoWindows[key].content);
             infoWindow.open(map, marker);
         });
     });
 
+    // Add a listener to enforce a minimum zoom level after the map is resized to fit all markers
+    google.maps.event.addListenerOnce(map, 'bounds_changed', function(){
+        if (this.getZoom() > 15 ) {
+            this.setZoom(15);
+        }
+    });
+
+    // Automatically ensure all markers fit on the map
+    // see https://wrightshq.com/playground/placing-multiple-markers-on-a-google-map-using-api-3/
+    map.fitBounds(bounds);
 
     // Add the map, markers, and infoWindow objects to a global variable
     gmMaps[mapId] = {map: map, markers: markers, infoWindow: infoWindow};
