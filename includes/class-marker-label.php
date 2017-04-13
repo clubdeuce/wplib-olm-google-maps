2fbf57ef5aff711dc8ae8e6b9049c61ae74dd702
Daryl Lozupone <daryl@newclarity.net>
2017-04-13 14:31:09 -0400

    Add @method annotations

--- includes/class-marker-label.php
+++ includes/class-marker-label.php
@@ -9,6 +9,13 @@ namespace Clubdeuce\WPLib\Components\GoogleMaps;
  * @property Marker_Label_View  $view
  * @mixin    Marker_Label_Model
  * @mixin    Marker_Label_View
+ * @method   string       color()
+ * @method   string|array options()
+ * @method   string       font_family()
+ * @method   string       font_size()
+ * @method   string       font_weight()
+ * @method   string       text()
+ * @method   string       json_object()
  */
 class Marker_Label extends \WPLib_Item_Base {
 
