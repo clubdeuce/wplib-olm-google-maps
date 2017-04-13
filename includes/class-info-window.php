911798e735df34d8ee3f9e083a50766117ef53f4
Daryl Lozupone <daryl@newclarity.net>
2017-04-13 15:55:47 -0400

    Add @method annotations to Info_Window class

--- includes/class-info-window.php
+++ includes/class-info-window.php
@@ -9,6 +9,10 @@ namespace Clubdeuce\WPLib\Components\GoogleMaps;
  * @property Info_Window_View  $view
  * @mixin    Info_Window_Model
  * @mixin    Info_Window_View
+ * @method   string content()
+ * @method   int    pixel_offset()
+ * @method   array  position()
+ * @method   int    max_width()
  * @link     https://developers.google.com/maps/documentation/javascript/infowindows
  */
 class Info_Window extends \WPLib_Item_Base {
