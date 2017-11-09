f77fd7fca13302bbc90fc64acb37b1e3cb635ef3
Daryl Lozupone <daryl@newclarity.net>
2017-11-09 15:31:04 -0500

    Implement testing task

--- RoboFile.php
+++ RoboFile.php
@@ -7,4 +7,48 @@
 class RoboFile extends \Robo\Tasks
 {
     // define public methods as commands
+	function tests()
+	{
+
+		$this->taskExec('mysql -e "CREATE DATABASE IF NOT EXISTS test_db"')->run();
+		$this->taskExec('mysql -e "GRANT ALL ON test_db.* to \'root\'@\'%\'"')->run();
+		$this->taskSvnStack()
+			->checkout('https://develop.svn.wordpress.org/tags/4.8.3 wp-tests')
+			->run();
+
+		$this->setTestConfig();
+		$this->phpunit();
+
+	}
+
+	function phpunit()
+	{
+		$this->taskPhpUnit('vendor/bin/phpunit')
+		     ->configFile('tests/phpunit.xml.dist')
+		     ->run();
+	}
+
+	private function setTestConfig()
+	{
+
+		if (file_exists('wp-tests/wp-tests-config-sample.php')) {
+			copy('wp-tests/wp-tests-config-sample.php', 'wp-tests/wp-tests-config.php');
+		}
+
+		$this->taskReplaceInFile( 'wp-tests/wp-tests-config.php')
+			->from('youremptytestdbnamehere')
+			->to('test_db')
+			->run();
+
+		$this->taskReplaceInFile( 'wp-tests/wp-tests-config.php')
+			->from('yourusernamehere')
+			->to('root')
+			->run();
+
+		$this->taskReplaceInFile( 'wp-tests/wp-tests-config.php')
+			->from('yourpasswordhere')
+			->to('')
+			->run();
+	}
+
 }
