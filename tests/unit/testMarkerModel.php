0a87c9f92c9b015cb1600a27cd315077609cd940
Daryl Lozupone <daryl@newclarity.net>
2017-11-09 19:51:30 -0500

    Add Marker_Label_Model tests

--- tests/unit/testMarkerModel.php
+++ tests/unit/testMarkerModel.php
@@ -12,7 +12,7 @@ use Mockery\Mock;
  * Class TestMarkerModel
  * @package            Clubdeuce\WPLib\Components\GoogleMaps\Tests\UnitTests
  * @coversDefaultClass Clubdeuce\WPLib\Components\GoogleMaps\Marker_Model
- * @group              marker
+ * @group              Marker
  */
 class TestMarkerModel extends TestCase {
 
